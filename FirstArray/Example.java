0692cca1d46195f3742fed01b7dec2df71c47859
12154 <joshua.gelfand@vtsd.com>
2022-12-19 07:57:44 -0500

    example

---
+++ FirstArray/Example.java
@@ -0,0 +1,19 @@
+package FirstArray;
+
+public class Example{
+    public static void main(String args[]){
+
+        //Arrays store one datatype
+        int Numberofchristmassgifts[];
+        Numberofchristmassgifts = new int[2];
+
+        Numberofchristmassgifts[0] = 7;
+        Numberofchristmassgifts[1] = 3;
+
+        System.out.println(Numberofchristmassgifts[0]);
+        System.out.println(Numberofchristmassgifts[1]);
+
+
+    }
+
+}
